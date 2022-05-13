fee6f0802cc46f54290ee50141c23e0fb7c84896
WatchDogg27 <96317430+WatchDogg27@users.noreply.github.com>
2022-05-13 13:10:56 +0100

    Add files via upload

---
+++ Javascript01/index.js
@@ -0,0 +1,133 @@
+//Design a for loop that returns an array of even numbers from 1-100
+
+var Arraylist[] {
+    for ( int i = 0; i< 100; i++) {
+        if (Arraylist[i] % 2 ===0) {
+            return [Arraylist];
+        }
+    }
+}
+
+
+
+
+
+
+
+
+//Create a function that returns even numbers between two numbers A and B
+
+function EvenNumbers(){
+    let list[]
+    var A = 4
+    var B = 30
+
+    for ( var i = A; i< B; i++) {
+        if (list[i] % 2 ===0 ) {
+            return [list]
+        }
+    }
+
+}
+
+
+
+
+
+
+
+//When would you use a conditional statement? (a)when you want to reuse a set of statements multiple times (b)when you want your code to choose between multiple numbers © when you want to group data together (d) when you want to loop through a group of statements.
+
+//Answer : (a), (b), (d)
+
+
+
+
+
+//Create a function with a function name firstFunction( ) and return the sum of two integers(numbers)
+
+function firstFunction(a ,b){
+
+    let sum = a + b;
+    return sum
+}
+firstFunction(23 ,56)
+
+
+
+
+
+
+//Create a function with a function name printSum( ) and print the sum of two numbers.
+
+function printSum(a ,b){
+
+    let sum = a + b;
+    print (sum);
+}
+printSum()
+
+
+
+
+
+
+//Create a function that returns an array of the names of 10 of your course mates
+
+function MyArray(names){
+    names = ['Tobi','Funmibi','Abi','Elijah','Bode','Mary','Emma','David','Mueeuz','Tolu'];
+    return names;
+}
+
+
+
+
+
+
+
+//Answer the following:
+
+//Add an element to the end of this array [1,2,4]
+
+let array = [1,2,4];{   
+    array.push(7);
+    return array
+}
+
+
+
+
+
+
+//Remove the last element in this array [1,2,3,8]
+
+let arr = [1,2,3,8];{   
+    arr.pop(8);
+    return arr
+}
+
+
+
+
+
+
+//Remove the first element in this array [23.77,80,45]
+
+let Arr1 = [23.77,80,4];{   
+    Arr1.shift(23.77);
+    return Arr1
+}
+
+
+
+
+
+
+
+//Add an element to the beginning of the array [[20,30,80],[10,50,55]]
+
+let Array2 = [[20,30,80],[10,50,55]] ;{   
+    Array2.unshift(23);
+    return Array2
+}
+
